47cbb0e7e56555e6b709b330c330af99cdad60f2
likaizhu <likaizhu@xdf.cn>
2020-11-07 00:56:13 +0800

    update template

--- src/App.vue
+++ src/App.vue
@@ -6,60 +6,46 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/helloWorld'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
   },
-  data() {
+  data () {
     return {
-      pass: "111",
+      pass: '111',
       arr: [1, 2, 3],
       obj: {
         name: '1',
         age: 1,
-        name: '22',
       },
-      c:{ a:1 },
+      c: { a: 1 },
     }
   },
-  mounted() {
-    const name = this.pass
-    this.getPass(name)
+  mounted () {
+    // const name = this.pass
+    // this.getPass(name)
   },
   methods: {
-    getPass(name) {
-      
-      let a
-      a = 12
-      let pass = 2
-      let res = 3
-      if (a === pass) {
-        res = 2
-      }
-      const c = 12
-      c = 0
-      
-      pass = this.pass + a
-      res = 1 + 2
-      console.log(pass)
-      console.log(name)
-      console.log(res)
-    },
-    he() {},
+    // getPass(name) {
+    //   let pass = 2
+    //   let res = 3
+    //   if (a === pass) {
+    //     res = 2
+    //   }
+    //   const c = 12
+    //   c = 0
+    //   pass = this.pass + a
+    //   res = 1 + 2
+    //   console.log(pass)
+    //   console.log(name)
+    //   console.log(res)
+    // },
+    // he() {},
   },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less" src="./style/index.less"></style>
